adfbf8eee2b644479bf73bb06ba125d503a162e4
Aneta <sopaaneta@gmail.com>
2023-05-07 23:39:09 +0200

    Remove set the month and day of the week

--- src/Component/Component.js
+++ src/Component/Component.js
@@ -4,9 +4,8 @@ import styles from './component.module.css';
 import Header from './Header/Header';
 import Timer from './Timer/Timer';
 
-// const holidayTime = new Date('Tuesday August 01 2023 12:12:12');
-
 function Component() {
+  // set the default value
   const [days, setDays] = useState(0);
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
@@ -15,78 +14,26 @@ function Component() {
   const deadline = 'August, 01, 2023';
 
   const getTime = () => {
+    // parse - converts the declared string deadline
     const time = Date.parse(deadline) - Date.now();
-
-    const deadlineTime = new Date(deadline);
-
-    console.log({
-      'Date.parse(deadline)': deadlineTime.getHours(),
-      'Date.now()': new Date().getHours(),
-    });
-
+    // calculate values for variables
     setDays(Math.floor(time / (1000 * 60 * 60 * 24)));
     setHours(Math.floor((time / (1000 * 60 * 60)) % 24));
     setMinutes(Math.floor((time / 1000 / 60) % 60));
     setSeconds(Math.floor((time / 1000) % 60));
   };
 
+  // get the timer to render after every second
   useEffect(() => {
     const interval = setInterval(() => getTime(deadline), 1000);
     return () => clearInterval(interval);
   }, []);
 
-  // const data = new Date();
-
-  // const monthNames = [
-  //   'January',
-  //   'February',
-  //   'March',
-  //   'April',
-  //   'May',
-  //   'June',
-  //   'July',
-  //   'August',
-  //   'September',
-  //   'October',
-  //   'November',
-  //   'December',
-  // ];
-
-  // const dayNames = [
-  //   'Sunday',
-  //   'Monday',
-  //   'Tuesday',
-  //   'Wednesday',
-  //   'Thursday',
-  //   'Friday',
-  //   'Saturday',
-  // ];
-  // const dayName = dayNames[data.getDay()];
-  // const monthName = monthNames[data.getMonth()];
-
-  // useEffect(() => {
-  //   const timer = setInterval(() => {
-  //     setTime(getTime());
-  //   }, 1000);
-
-  //   return () => {
-  //     clearInterval(timer);
-  //   };
-  // }, []);
-
   return (
-    <div className={styles.container}>
+    <div>
       <img className={styles.img} alt="img" src={image} />
       <Header />
-      <Timer
-        // monthName={monthName}
-        // dayName={dayName}
-        // month={time}
-        days={days}
-        hours={hours}
-        minutes={minutes}
-        seconds={seconds}
-      />
+      <Timer days={days} hours={hours} minutes={minutes} seconds={seconds} />
     </div>
   );
 }
