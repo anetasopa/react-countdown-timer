fb6d3d7f2a75c2e36948787a3658d1f46cadcad2
Aneta <sopaaneta@gmail.com>
2023-05-08 00:27:41 +0200

    Add styles

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -4,7 +4,13 @@ function Header() {
   return (
     <div className={styles.container}>
       <h1>Countdown Timer</h1>
-      <span className={styles.span}>Waiting time until the holidays...</span>
+      <span className={styles.span}>
+        Waiting time until the{' '}
+        <b>
+          <i>holidays</i>
+        </b>
+        ...
+      </span>
     </div>
   );
 }
--- src/Component/Timer/Timer.js
+++ src/Component/Timer/Timer.js
@@ -3,10 +3,33 @@ import styles from './timer.module.css';
 function Timer({ days, hours, minutes, seconds }) {
   return (
     <div className={styles.container}>
-      <span>{days < 10 ? '0' + days : days} : </span>
-      <span>{hours < 10 ? '0' + hours : hours} : </span>
-      <span>{minutes < 10 ? '0' + minutes : minutes} : </span>
-      <span>{seconds < 10 ? '0' + seconds : seconds}</span>
+      <div className={styles.contents}>
+        <div className={styles.block}>
+          <span>{days < 10 ? '0' + days : days}</span>
+          <span className={styles.containerName}>DAYS</span>
+        </div>
+        <span> : </span>
+      </div>
+      <div className={styles.contents}>
+        <div className={styles.block}>
+          <span>{hours < 10 ? '0' + hours : hours}</span>
+          <span className={styles.containerName}>HOURS</span>
+        </div>
+        <span> : </span>
+      </div>
+      <div className={styles.contents}>
+        <div className={styles.block}>
+          <span>{minutes < 10 ? '0' + minutes : minutes}</span>
+          <span className={styles.containerName}>MINUTES</span>
+        </div>
+        <span> : </span>
+      </div>
+      <div className={styles.contents}>
+        <div className={styles.block}>
+          <span>{seconds < 10 ? '0' + seconds : seconds}</span>
+          <span className={styles.containerName}>SECONDS</span>
+        </div>
+      </div>
     </div>
   );
 }
